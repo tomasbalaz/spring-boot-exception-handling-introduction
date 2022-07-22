6f1147c907ab21c341d2c401f933b4a0878aea40
Tomas Balaz <balaztomas0@gmail.com>
2022-07-22 10:22:30 +0200

    Created spring-boot-validation-exception-handling application

---
+++ src/test/java/sk/balaz/springbootvalidationexceptionhandling/SpringBootValidationExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springbootvalidationexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationExceptionHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
