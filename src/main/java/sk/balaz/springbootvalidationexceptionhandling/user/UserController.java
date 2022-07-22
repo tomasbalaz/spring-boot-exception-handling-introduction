3078ef3b4408da282d58959da2b553c2f5308601
Tomas Balaz <balaztomas0@gmail.com>
2022-07-22 10:54:35 +0200

    Enable Spring boot request validation with @Valid annotation

--- src/main/java/sk/balaz/springbootvalidationexceptionhandling/user/UserController.java
+++ src/main/java/sk/balaz/springbootvalidationexceptionhandling/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
@@ -16,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> saveUser(@RequestBody UserRequestDTO userRequest) {
+    public ResponseEntity<User> saveUser(@RequestBody @Valid UserRequestDTO userRequest) {
         return new ResponseEntity<>(
                 userService.saveUser(userRequest),
                 HttpStatus.CREATED);
--- src/main/java/sk/balaz/springbootvalidationexceptionhandling/user/UserRequestDTO.java
+++ src/main/java/sk/balaz/springbootvalidationexceptionhandling/user/UserRequestDTO.java
@@ -3,20 +3,24 @@ package sk.balaz.springbootvalidationexceptionhandling.user;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 @Builder
 public class UserRequestDTO {
 
+    @NotBlank
+    @NotNull
     private String name;
 
+    @Email
     private String email;
 
+    @NotBlank
+    @NotNull
     private String mobile;
 
+    @Min(value = 1, message = "Invalid Age: Equals to zero or Less than zero")
+    @Max(value = 100, message = "Invalid Age: Exceeds 100 years")
     private int age;
 }
